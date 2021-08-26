18ef216ded85437b764e25f4e5712dfe491071bb
mayaAtanasova <mayaatanasova@gmail.com>
2021-08-26 15:10:58 +0300

    Updated code

--- projects/aBitMoreComplicatedCalc/script.js
+++ projects/aBitMoreComplicatedCalc/script.js
@@ -8,6 +8,40 @@ function clearInput() {
 
 function calculate() {
     let input = document.getElementsByName('inputfield')[0].value;
-    let result = eval(input);
+    let result = evaluate(input);
     document.getElementsByName('inputfield')[0].value = result;
+}
+
+function evaluate(string) {
+    const actions = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
+    }
+    const nrsPatt = /[^\+\-\*\/]+/g;
+    const actionsPatt = /[\+\-\*\/]/g;
+    let nrs = [...string.matchAll(nrsPatt)].map(x => Number(x[0]));
+    let commands = [...string.matchAll(actionsPatt)].map(x => x[0]);
+    while (nrs.length > 1) {
+        if (commands.includes('*') || commands.includes('/')) {
+            let opIndex;
+            if(commands.indexOf('*') == -1){
+                opIndex = commands.indexOf('/');
+            }else if(commands.indexOf('/') == -1){
+                opIndex = commands.indexOf('*');
+            }else{
+                opIndex = Math.min(commands.indexOf('*'), commands.indexOf('/'));
+            }
+            let temp = actions[commands[opIndex]](nrs[opIndex], nrs[opIndex + 1]);
+            commands.splice(opIndex, 1);
+            nrs.splice(opIndex, 2, temp);
+        } else {
+            opIndex = 0;
+            let temp = actions[commands[opIndex]](nrs[opIndex], nrs[opIndex + 1]);
+            commands.splice(opIndex, 1);
+            nrs.splice(opIndex, 2, temp);
+        }
+    }
+    return nrs[0];
 }
